d7cecd38dcf90767c856041553b8ebb061b4c499
Andreas Källberg <andreas@svalna.se>
2020-06-14 21:50:38 +0200

    Use developPackage

--- default.nix
+++ default.nix
@@ -1,2 +1,1 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc883" }:
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./gf-core.nix { }
+{ pkgs ? import <nixpkgs> {} }: pkgs.haskellPackages.developPackage { root = ./.; }
