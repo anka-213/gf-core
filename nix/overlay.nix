dadebf0646b65047002c02d36fd3d0d6f2c32818
Andreas Källberg <anka.213@gmail.com>
2020-09-23 10:29:02 +0200

    Split overlay to separate file

---
+++ nix/overlay.nix
@@ -0,0 +1,14 @@
+pkgs: oldpkgs: {
+  haskellPackages = oldpkgs.haskellPackages.override {
+    overrides = haskellPackagesNew: haskellPackagesOld: {
+      # site = haskellPackagesNew.callPackage ./site.nix {};
+      # gf-core = (haskellPackagesNew.callPackage ./gf-core.nix {}).overrideAttrs(old: {
+      #   configureFlags = "-f c-runtime";
+      #   # jailbreak = true; # jailbreak dependecies
+      # });
+      gf-core = haskellPackagesNew.callPackage ./gf-core-c.nix {};
+      gf-c-bindings = haskellPackagesNew.callPackage ./haskell-bind.nix {};
+    };
+  };
+  gf-pgf = pkgs.callPackage ./c-runtime.nix {};
+}
--- nix/release.nix
+++ nix/release.nix
@@ -1,20 +1,6 @@
 let
-  myOverlay = 
-     pkgs: oldpkgs: {
-      haskellPackages = oldpkgs.haskellPackages.override {
-        overrides = haskellPackagesNew: haskellPackagesOld: {
-          # site = haskellPackagesNew.callPackage ./site.nix {};
-          # gf-core = (haskellPackagesNew.callPackage ./gf-core.nix {}).overrideAttrs(old: {
-          #   configureFlags = "-f c-runtime";
-          #   # jailbreak = true; # jailbreak dependecies
-          # });
-          gf-core = haskellPackagesNew.callPackage ./gf-core-c.nix {};
-          gf-c-bindings = haskellPackagesNew.callPackage ./haskell-bind.nix {};
-        };
-      };
-      gf-pgf = pkgs.callPackage ./c-runtime.nix {};
-    };
-  
+  myOverlay = import ./overlay.nix;
+
   pkgs = import <nixpkgs> { overlays = [ myOverlay]; };
 # in pkgs.haskellPackages.gf-core
 # in pkgs.asdfasdf
